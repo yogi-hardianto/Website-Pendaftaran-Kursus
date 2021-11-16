fe2f5c287221b372353a1dd7474d6150c590f84b
Dian-indah <dianzurindah.aks17@gmail.com>
2021-11-16 22:02:13 +0700

    login petugas

---
+++ View/login_petugas.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>You Login Template</title>
+	<!-- Site favicon -->
+	<link rel="shortcut icon" href="../assets/images/favicon.ico">
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700" rel="stylesheet">
+	<!-- Icon Font -->
+	<link rel="stylesheet" href="../assets/fonts/ionicons/css/ionicons.css">
+	<!-- Text Font -->
+	<link rel="stylesheet" href="../assets/fonts/font.css">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="../assets/css/bootstrap.css">
+	<!-- Normal style CSS -->
+	<link rel="stylesheet" href="../assets/css/style.css">
+	<!-- Normal media CSS -->
+	<link rel="stylesheet" href="../assets/css/media.css">
+</head>
+<body>
+
+	<main class="cd-main">
+		<section class="cd-section index2 visible">
+			<div class="cd-content style2">
+				<div class="login-box">
+					<div class="login-form-slider">
+						<!-- login slide start -->
+						<div class="login-slide slide">
+							<div class="d-flex height-100-percentage">
+								<div class="align-self-center width-100-percentage">
+									<h3>Sign In</h3>
+									<form>
+										<div class="form-group user-name-field">
+											<input type="text" class="form-control" placeholder="User name">
+											<div class="field-icon"><i class="ion-person"></i></div>
+										</div>
+										<div class="form-group margin-bottom-30px forgot-password-field">
+											<input type="password" class="form-control" placeholder="Password">
+											<div class="field-icon"><i class="ion-locked"></i></div>
+											<a href="javascript:;" class="forgot-password-click">Forgot?</a>
+										</div>
+										<div class="form-group sign-in-btn">
+											<input type="submit" class="submit" value="Sign In">
+										</div>
+									</form>
+									<div class="sign-up-txt">
+										Don't have an account? <a href="javascript:;" class="sign-up-click">Signup</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- login slide end -->
+						<!-- signup slide start -->
+						<div class="signup-slide slide">
+							<div class="d-flex height-100-percentage">
+								<div class="align-self-center width-100-percentage">
+									<h3>Sign Up</h3>
+									<form>
+										<div class="form-group user-name-field">
+											<input type="text" class="form-control" placeholder="Name">
+											<div class="field-icon"><i class="ion-person"></i></div>
+										</div>
+										<div class="form-group user-name-field">
+											<input type="text" class="form-control" placeholder="Email">
+											<div class="field-icon"><i class="ion-ios-email"></i></div>
+										</div>
+										<div class="form-group">
+											<input type="password" class="form-control" placeholder="Password">
+											<div class="field-icon"><i class="ion-locked"></i></div>
+										</div>
+										<div class="form-group margin-bottom-30px">
+											<input type="password" class="form-control" placeholder="Confirm Password">
+											<div class="field-icon"><i class="ion-locked"></i></div>
+										</div>
+										<div class="form-group sign-in-btn">
+											<input type="submit" class="submit" value="Sign Up">
+										</div>
+									</form>
+									<div class="sign-up-txt">
+										if you have an account? <a href="javascript:;" class="login-click">login</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- signup slide end -->
+						<!-- forgot password slide start -->
+						<div class="forgot-password-slide slide">
+							<div class="d-flex height-100-percentage">
+								<div class="align-self-center width-100-percentage">
+									<h3>Forgot Password</h3>
+									<form>
+										<label class="label">Enter your email address to reset your password</label>
+										<div class="form-group user-name-field">
+											<input type="text" class="form-control" placeholder="Email">
+											<div class="field-icon"><i class="ion-ios-email"></i></div>
+										</div>
+										<div class="form-group sign-in-btn">
+											<input type="submit" class="submit" value="Submit">
+										</div>
+									</form>
+									<div class="sign-up-txt">
+										if you remember your password? <a href="javascript:;" class="login-click">login</a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- forgot password slide end -->
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	<div id="cd-loading-bar" data-scale="1"></div>
+	<!-- JS File -->
+	<script src="../assets/js/modernizr.js"></script>
+	<script type="text/javascript" src="../assets/js/jquery.js"></script>
+	<script type="text/javascript" src="../assets/js/popper.min.js"></script>
+	<script type="text/javascript" src="../assets/js/bootstrap.js"></script>
+	<script src="../assets/js/velocity.min.js"></script>
+	<script type="text/javascript" src="../assets/js/script.js"></script>
+</body>
+</html>
