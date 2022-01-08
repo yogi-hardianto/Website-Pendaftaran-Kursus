7e860b0bb077fa53a86ba8cc96584826f4e07756
Dian-Indah <dianzurindah.aks17@gmail.com>
2022-01-08 23:54:49 +0700

    update siswa

--- View/menu/menusiswa.php
+++ View/menu/menusiswa.php
@@ -1,112 +1,52 @@
-<!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Siswa | Dashboard</title>
-
-    <!-- Custom fonts for this template-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/1c0f91b923.js" crossorigin="anonymous"></script>
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    
-    <!-- Custom styles for this template-->
-    <!-- <link rel="stylesheet" href="assets/css/stylesiswa.css"> -->
-    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-
+    <title>Ruang Belajar | SISWA</title>
 </head>
+<body>
+    <!-- nav awal -->
+    <nav class="navbar navbar-expand-lg navbar-light nav-tabs bg-secondary">
+        <a class="navbar-brand text-white" href="#">Ruang Belajar | </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <!-- menu navbar -->
+            <div class="collapse navbar-collapse " id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active ml-3">
+                        <a class="nav-link text-white font-weight-normal" href="index.php?page=siswa&aksi=view">Dashboard</a>
+                    </li>
+                    <li class="nav-item active ml-3">
+                        <a class="nav-link text-white font-weight-normal" href="index.php?page=siswa&aksi=profil">Profil</a>
+                    </li>
+                    <li class="nav-item active ml-3">
+                        <a class="nav-link text-white font-weight-normal" href="index.php?page=siswa&aksi=daftarkursus"><i class="fas fa-clipboard-list mr-2"></i>Daftar Kursus</a>
+                    </li>
+                    <li class="nav-item active ml-3">
+                        <a class="nav-link text-white font-weight-normal" href="#">Jadwal Kursus</a>
+                    </li>
+                    <li class="nav-item active ml-3">
+                        <a class="nav-link text-white font-weight-normal" href="#">Pembayaran</a>
+                    </li>                 
+                </ul>
+            </div>
+        <!-- end menu navbar -->
 
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon ">
+        <!-- user id -->
+        <div class="form-inline">
+            <a class="d-flex align-items-center float-right ml-auto text-white " href="index.html">
+                <div class="sidebar-brand-icon">
                     <i class="fas fa-user-graduate"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">=NamaSiswa=</div>
             </a>
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-            <a class="nav-link" href="index.php?page=siswa&aksi=view">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-            
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-            <a class="nav-link" href="index.php?page=siswa&aksi=profil">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Profil</span></a>
-            </li>
-
-            <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-            <a class="nav-link" href="index.php?page=siswa&aksi=daftarkursus">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Daftar Kursus</span></a>
-            </li>
-
-            <!-- Nav Item - Charts -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?page=siswa&aksi=daftarkursus">
-                <i class="fas fa-calendar-week mr-2"></i>
-                    <span>Jadwal Kursus</span></a>
-            </li>
-            
-            <!-- Pembayaran -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.php?page=siswa&aksi=daftarkursus">
-                <i class="fas fa-money-bill mr-2"></i>
-                    <span>Pembayaran</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
-        <!-- End of Sidebar -->
-
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Ruang Belajar 2021</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
         </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
+        <!-- end user id -->
+    </nav>
+    <!-- end nav awal -->
 </body>
 </html>
--- View/siswa/index.php
+++ View/siswa/index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Siswa | Dashboard</title>
+    <title></title>
 
     <!-- Custom fonts for this template-->
     <script src="https://kit.fontawesome.com/1c0f91b923.js" crossorigin="anonymous"></script>
@@ -29,36 +29,9 @@
     <div id="content-wrapper" class="d-flex flex-column">
 
     <!-- Main Content -->
-    <div id="content">
-
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-            <!-- Sidebar Toggle (Topbar) -->
-            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                <i class="fa fa-bars"></i>
-            </button>
-            <!-- Topbar Navbar -->
-            <ul class="navbar-nav ml-auto">
-            
-                <!-- Nav Item - User Information -->
-                <li class="nav-item dropdown no-arrow">
-                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"> =NamaSiswa=</span>
-                        <img class="img-profile rounded-circle"
-                            src="assets/img/people.jpg">
-                    </a>
-
-                </li>
-                
-            </ul>
-
-        </nav>
-        <!-- End of Topbar -->
-
+    <div id="content mt-3">
         <!-- Begin Page Content -->
-        <div class="container-fluid">
+        <div class="container-fluid mt-3">
 
             <!-- Page Heading -->
             <h3><i class="fas fa-fw fa-tachometer-alt"></i> Dashboard <hr></h3>
